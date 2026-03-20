06e5c6b2b15f5a509d04d829caaf2f820ce3c0ec
piomin <piotr.minkowski@gmail.com>
2026-03-20 09:56:37 +0100

    fix issues

--- src/main/java/pl/piomin/service/kubemq/model/Order.java
+++ src/main/java/pl/piomin/service/kubemq/model/Order.java
@@ -1,17 +1,8 @@
 package pl.piomin.service.kubemq.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@Builder
-@ToString
 public class Order implements Serializable {
 
     private OrderType type;
@@ -22,4 +13,85 @@ public class Order implements Serializable {
     private String id;
     private OrderStatus status;
 
+    public Order() {
+    }
+
+    public Order(OrderType type, Integer accountIdFrom, Integer accountIdTo, LocalDateTime date, int amount, String id, OrderStatus status) {
+        this.type = type;
+        this.accountIdFrom = accountIdFrom;
+        this.accountIdTo = accountIdTo;
+        this.date = date;
+        this.amount = amount;
+        this.id = id;
+        this.status = status;
+    }
+
+    public OrderType getType() {
+        return type;
+    }
+
+    public void setType(OrderType type) {
+        this.type = type;
+    }
+
+    public Integer getAccountIdFrom() {
+        return accountIdFrom;
+    }
+
+    public void setAccountIdFrom(Integer accountIdFrom) {
+        this.accountIdFrom = accountIdFrom;
+    }
+
+    public Integer getAccountIdTo() {
+        return accountIdTo;
+    }
+
+    public void setAccountIdTo(Integer accountIdTo) {
+        this.accountIdTo = accountIdTo;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "type=" + type +
+                ", accountIdFrom=" + accountIdFrom +
+                ", accountIdTo=" + accountIdTo +
+                ", date=" + date +
+                ", amount=" + amount +
+                ", id='" + id + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
